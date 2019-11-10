749fe19ee880244203ae223c05dee226b5445061
Szofi <nyafologus@gmail.com>
2019-11-10 11:02:46 +0100

    Handle errors in Wiki fetch

--- src/App.js
+++ src/App.js
@@ -15,24 +15,38 @@ class App extends Component {
 
   requestWikiData() {
     let wikiData = [];
+    let noData = [];
     this.state.mylocations.map((location) => {
       // fetches data from Wikipedia API
-      return fetch(
-        `https://en.wikipedia.org/w/api.php?&action=query&list=search&prop=extracts&titles&format=json&origin=*&srlimit=1&srsearch=${location.name}`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          // generates custom URL for more information
-          let url = encodeURI(`https://en.wikipedia.org/wiki/${data.query.search['0'].title}`);
-          let e = {
-            text: data.query.search['0'].snippet,
-            id: location.id,
-            url: url,
-            visitWiki: 'More information'
-          };
-          wikiData.push(e);
-          this.setState({ data: wikiData });
-        });
+      return (
+        fetch(
+          `https://en.wikipedia.org/w/api.php?&action=query&list=search&prop=extracts&titles&format=json&origin=*&srlimit=1&srsearch=${location.name}`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            // generates custom URL for more information
+            let url = encodeURI(`https://en.wikipedia.org/wiki/${data.query.search['0'].title}`);
+            let e = {
+              text: data.query.search['0'].snippet,
+              id: location.id,
+              url: url,
+              visitWiki: 'More information'
+            };
+            wikiData.push(e);
+            this.setState({ data: wikiData });
+          })
+          // handles error in case of fetching failure
+          .catch(() => {
+            let e = {
+              id: location.id,
+              text: 'Unfortunately an error occured while obtaining data from Wikipedia',
+              visitWiki: 'Please refresh'
+            };
+            console.log('ERROR! Wiki API could not load.');
+            noData.push(e);
+            this.setState({ data: noData });
+          })
+      );
     });
   }
 
