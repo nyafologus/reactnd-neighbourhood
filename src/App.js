5abd315175da0f83abb7f8daffc21570686214dc
Szofi <nyafologus@gmail.com>
2019-11-10 11:01:57 +0100

    Fetch data from Wikipedia

--- src/App.js
+++ src/App.js
@@ -8,11 +8,38 @@ class App extends Component {
     super(props);
     this.state = {
       // loads Magical Places array from external json file
-      mylocations: require('./Data/Places.json')
+      mylocations: require('./Data/Places.json'),
+      data: []
     };
   }
 
-  // componentDidMount() {}
+  requestWikiData() {
+    let wikiData = [];
+    this.state.mylocations.map((location) => {
+      // fetches data from Wikipedia API
+      return fetch(
+        `https://en.wikipedia.org/w/api.php?&action=query&list=search&prop=extracts&titles&format=json&origin=*&srlimit=1&srsearch=${location.name}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          // generates custom URL for more information
+          let url = encodeURI(`https://en.wikipedia.org/wiki/${data.query.search['0'].title}`);
+          let e = {
+            text: data.query.search['0'].snippet,
+            id: location.id,
+            url: url,
+            visitWiki: 'More information'
+          };
+          wikiData.push(e);
+          this.setState({ data: wikiData });
+        });
+    });
+  }
+
+  componentDidMount() {
+    // starts requesting data from Wikipedia API
+    this.requestWikiData();
+  }
 
   render() {
     return (
