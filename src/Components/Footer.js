cae9768e2ed5d46b3669a78bfb01af999e0cb066
Szofi <nyafologus@gmail.com>
2019-11-10 11:41:25 +0100

    Style Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,15 +7,15 @@ const Footer = () => (
       Copyright (c) 2019 <Link to='/'>Budapest by Night - React x Google Maps x Wiki App</Link>
     </p>
     <div>
-      Icons made by{' '}
+      <span>Icons made by</span>
       <a href='http://www.freepik.com' title='Freepik'>
         Freepik
-      </a>{' '}
-      from{' '}
+      </a>
+      <span>from</span>
       <a href='https://www.flaticon.com/' title='Flaticon'>
         www.flaticon.com
-      </a>{' '}
-      is licensed by{' '}
+      </a>
+      <span>is licensed by</span>
       <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0'>
         CC 3.0 BY
       </a>
